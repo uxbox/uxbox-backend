4e6ce06c8a1830428911e4bbfc524529f0b7e874
Andrey Antukh <niwi@niwi.nz>
2016-02-06 20:32:53 +0200

    Move developer stuf from user to frontend.

--- src/uxbox/frontend.clj
+++ src/uxbox/frontend.clj
@@ -1,7 +1,48 @@
 (ns uxbox.frontend
-  (:require [mount.core :as mount]
+  (:require [clojure.tools.namespace.repl :as repl]
+            [mount.core :as mount]
             [uxbox.frontend.server])
   (:gen-class))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Development Stuff
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- start
+  []
+  (mount/start))
+
+(defn- stop
+  []
+  (mount/stop))
+
+(defn- refresh
+  []
+  (stop)
+  (repl/refresh))
+
+(defn- refresh-all
+  []
+  (stop)
+  (repl/refresh-all))
+
+(defn- go
+  "starts all states defined by defstate"
+  []
+  (start)
+  :ready)
+
+(defn- reset
+  []
+  (stop)
+  (repl/refresh :after 'uxbox.frontend/start))
+
+;; (mount/in-clj-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Entry point (only for uberjar)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (defn -main
   [& args]
