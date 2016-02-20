d007d660256285089ba4c39e8e5e2fc192b448ec
Andrey Antukh <niwi@niwi.nz>
2016-02-20 21:32:18 +0200

    Add first test for services and transaction log.

--- test/uxbox/tests/test_txlog.clj
+++ test/uxbox/tests/test_txlog.clj
@@ -1,12 +1,19 @@
 (ns uxbox.tests.test-txlog
   "A txlog and services abstraction generic tests."
   (:require [clojure.test :as t]
+            [promesa.core :as p]
+            [uxbox.services.core :as usc]
+            [uxbox.services :as usv]
             [uxbox.tests.helpers :as th]))
 
 (t/use-fixtures :each th/database-reset)
 
-(t/deftest experiment-spec1
-  (t/is (= 1 1)))
+(defmethod usc/-novelty ::testype1
+  [data]
+  true)
 
-(t/deftest experiment-spec2
-  (t/is (= 1 1)))
+(t/deftest txlog-spec1
+  (let [data {:type ::testype1 :foo 1 :bar "baz"}
+        response (usv/novelty data)]
+    (t/is (p/promise? response))
+    (t/is (= 1 @response))))
