ad7615a72fbdc7325c7bcde5dedc89442cb41f9c
Andrey Antukh <niwi@niwi.nz>
2016-02-20 20:01:31 +0200

    Initial impl for auth handler.

--- src/uxbox/frontend/auth.clj
+++ src/uxbox/frontend/auth.clj
@@ -1,9 +1,26 @@
 (ns uxbox.frontend.auth
   (:require [catacumba.handlers.postal :as pc]
+            [uxbox.schema :as us]
             [uxbox.frontend.core :refer (-handler)]
+            [uxbox.services :as sv]
             [promesa.core :as p]))
 
-(defmethod -handler [:query :auth]
-  [context in-frame]
-  (p/alet [data {:message "hello world"}]
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Schema
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def +auth-frame-schema+
+  {:username [us/required us/string]
+   :password [us/required us/string]
+   :scope [us/required us/string]})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Handlers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defmethod -handler [:novelty :auth/login]
+  [context {:keys [data dest]}]
+  (p/alet [data (us/extract! +auth-frame-schema+ data)
+           data (assoc data :type dest)
+           resp (p/await (sv/novelty data))]
     (pc/frame data)))
