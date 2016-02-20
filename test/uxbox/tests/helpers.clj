286d2e8e8de9819bbb6adb1b06b0c0ae6cc53c49
Andrey Antukh <niwi@niwi.nz>
2016-02-20 21:26:24 +0200

    Make migrations non verbose on tests.

--- test/uxbox/tests/helpers.clj
+++ test/uxbox/tests/helpers.clj
@@ -2,17 +2,23 @@
     (:require [clojure.test :as t]
             [mount.core :as mount]
             [suricatta.core :as sc]
+            [uxbox.migrations :as umg]
             [uxbox.persistence :as up]
             [uxbox.config :as ucfg]))
 
 (def +config+ (ucfg/read-test-config))
 (def +ds+ (up/create-datasource (:database +config+)))
 
+(defn migrate
+  [& args]
+  (umg/migrate {:verbose false}))
+
 (defn database-reset
   [next]
   (with-open [conn (sc/context +ds+)]
     (sc/execute conn "drop schema if exists public cascade;")
     (sc/execute conn "create schema public;"))
-  (mount/start-with {#'uxbox.config/config +config+})
+  (with-redefs [uxbox.migrations/migrate migrate]
+    (mount/start-with {#'uxbox.config/config +config+}))
   (next))
 
