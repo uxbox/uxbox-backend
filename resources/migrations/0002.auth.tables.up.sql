06cc33c2d3128317108fba29ebc45f62f2a1b41e
Andrey Antukh <niwi@niwi.nz>
2016-02-23 17:07:09 +0200

    Remove tokens table.

--- resources/migrations/0002.auth.tables.up.sql
+++ resources/migrations/0002.auth.tables.up.sql
@@ -8,13 +8,6 @@ CREATE TABLE IF NOT EXISTS users (
   password text
 ) WITH (OIDS=FALSE);
 
-CREATE TABLE IF NOT EXISTS tokens (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  created_at timestamptz DEFAULT current_timestamp,
-  salt text,
-  user_id uuid references users(id)
-) WITH (OIDS=FALSE);
-
 CREATE UNIQUE INDEX users_username_idx
   ON users USING btree (username);
 
