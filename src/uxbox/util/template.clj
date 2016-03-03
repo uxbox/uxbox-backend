0fd6224dd0fc41b3b1e9064467d62600df499418
Andrey Antukh <niwi@niwi.nz>
2016-03-04 00:15:48 +0200

    Add license header to all files.

--- src/uxbox/util/template.clj
+++ src/uxbox/util/template.clj
@@ -1,4 +1,8 @@
-;; Copyright 2016 Andrey Antukh <niwi@niwi.nz>
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
 
 (ns uxbox.util.template
   "A lightweight abstraction over mustache.java template engine.
