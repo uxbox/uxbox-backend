ac25f1f072714ce7ef5d332433bc7b8a17315b4a
Andrey Antukh <niwi@niwi.nz>
2016-03-04 18:44:38 +0200

    Add missing not null props to user table fields.

--- resources/migrations/0002.auth.tables.up.sql
+++ resources/migrations/0002.auth.tables.up.sql
@@ -2,10 +2,9 @@ CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at timestamptz DEFAULT current_timestamp,
   modified_at timestamptz DEFAULT current_timestamp,
-  photo text,
-  username text,
-  email text,
-  password text
+  username text NOT NULL,
+  email text NOT NULL,
+  password text NOT NULL
 ) WITH (OIDS=FALSE);
 
 CREATE UNIQUE INDEX users_username_idx
