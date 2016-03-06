6a73cbf93856179e08002663e2cc10255c28fade
Andrey Antukh <niwi@niwi.nz>
2016-03-06 11:48:58 +0200

    Improve projects and pages migration file.

--- resources/migrations/0003.projects.and.pages.up.sql
+++ resources/migrations/0003.projects.and.pages.up.sql
@@ -3,24 +3,20 @@ CREATE TABLE IF NOT EXISTS projects (
   created_at timestamptz DEFAULT current_timestamp,
   modified_at timestamptz DEFAULT current_timestamp,
 
-  user uuid NOT NULL REFERENCES users(id),
-  name text NOT NULL,
-
-  width bigint NOT NULL,
-  height bigint NOT NULL,
-  layout text NOT NULL
+  "user" uuid NOT NULL REFERENCES users(id),
+  name text NOT NULL
 ) WITH (OIDS=FALSE);
 
 CREATE TABLE IF NOT EXISTS pages (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at timestamptz DEFAULT current_timestamp,
 
-  user uuid NOT NULL REFERENCES users(id),
+  "user" uuid NOT NULL REFERENCES users(id),
   project uuid NOT NULL REFERENCES projects(id),
   name text NOT NULL,
-  data text NOT NULL,
+  data text,
 
   width bigint NOT NULL,
   height bigint NOT NULL,
-  layout text NOT NULL,
+  layout text NOT NULL
 ) WITH (OIDS=FALSE);
