30483605559b28934095c9aa77914bb87f43b800
Andrey Antukh <niwi@niwi.nz>
2016-03-09 23:55:52 +0200

    Replace postal helpers with http helpers (tesing).

--- test/uxbox/tests/helpers.clj
+++ test/uxbox/tests/helpers.clj
@@ -3,7 +3,7 @@
   (:require [clojure.test :as t]
             [clj-http.client :as http]
             [buddy.core.codecs :as codecs]
-            [catacumba.handlers.postal :as pc]
+            [catacumba.serializers :as sz]
             [mount.core :as mount]
             [suricatta.core :as sc]
             [uxbox.migrations :as umg]
@@ -33,15 +33,21 @@
      (catch Exception e#
        (.getCause e#))))
 
-(defn send-frame
-  [uri frame]
-  (let [data (pc/-encode frame :application/transit+json)
-        headers {"content-type" "application/transit+json"}
-        params {:body data :headers headers}
-        response  (http/put uri params)]
-    (if (= (:status response) 200)
-      (-> (:body response)
-          (codecs/str->bytes)
-          (pc/-decode :application/transit+json))
-      (throw (ex-info "Wrong response" response)))))
 
+(defn- strip-response
+  [{:keys [status headers body]}]
+  (if (= (get headers "content-type") "application/transit+json")
+    [status (-> (codecs/str->bytes body)
+                (sz/decode :transit+json))]
+    [status body]))
+
+(defn post
+  [uri body]
+  (let [body (-> (sz/encode body :transit+json)
+                 (codecs/bytes->str))
+        headers {"content-type" "application/transit+json"}
+        params {:body body :headers headers}]
+    (try
+      (strip-response (http/post uri params))
+      (catch clojure.lang.ExceptionInfo e
+        (strip-response (ex-data e))))))
