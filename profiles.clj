a7983c2dc2441b52f0254e9fe8218d5df1404b23
Andrey Antukh <niwi@niwi.nz>
2016-03-13 14:15:08 +0200

    Update dependencies.

--- profiles.clj
+++ profiles.clj
@@ -2,6 +2,7 @@
  {:plugins [[lein-ancient "0.6.7"]]
   :dependencies [[clj-http "2.1.0"]]
   :main ^:skip-aot uxbox.main}
+
  :prod
  {:jvm-opts ^:replace ["-Xms4g" "-Xmx4g" "-XX:+UseG1GC"
                        "-XX:+AggressiveOpts" "-server"]}}
