cd9101bb605b8323beaf7f5a78991d770aa57d5a
Andrey Antukh <niwi@niwi.nz>
2016-03-24 11:05:23 +0200

    Improved querystring parameters parsing.

--- src/uxbox/util/querystring.clj
+++ src/uxbox/util/querystring.clj
@@ -5,11 +5,14 @@
 ;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
 
 (ns uxbox.util.querystring
-  (:require [uxbox.util.transit :as t]))
+  (:require [uxbox.util.transit :as t]
+            [buddy.core.codecs :as codecs]))
 
 (defn parse-params
   ([params] (parse-params params nil))
-  ([params {:keys [key] :as opts}]
+  ([params {:keys [key] :or {key :params} :as opts}]
    (if (contains? params key)
-     (t/decode (get params key))
+     (-> (get params key)
+         (codecs/base64->bytes)
+         (t/decode))
      {})))
