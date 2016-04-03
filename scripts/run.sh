d73b8c1c42cc4066626fe78d90f41cec2085db40
Andrey Antukh <niwi@niwi.nz>
2016-04-03 14:19:33 +0300

    Update run.sh script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-JVM_OPTS="-Xms4g -Xmx4g -XX:+UseG1GC -XX:+AggressiveOpts -server"
-java $JVM_OPTS -jar target/uxbox-backend-0.1.0-SNAPSHOT-standalone.jar -m uxbox.main
+# JVM_OPTS="-Xms4g -Xmx4g -XX:+UseG1GC -XX:+AggressiveOpts -server"
+# java $JVM_OPTS -jar target/uxbox-backend-0.1.0-SNAPSHOT-standalone.jar -m uxbox.main
+lein trampoline run -m uxbox.main
