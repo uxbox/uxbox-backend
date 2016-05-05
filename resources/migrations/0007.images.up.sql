0d09373ea11487b236213aae7107d4d58bf9713c
Andrey Antukh <niwi@niwi.nz>
2016-05-05 18:26:25 +0300

    Add improvements to images tables (migrations).

--- resources/migrations/0007.images.up.sql
+++ resources/migrations/0007.images.up.sql
@@ -2,11 +2,14 @@ CREATE TABLE IF NOT EXISTS image_collections (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at timestamptz NOT NULL DEFAULT clock_timestamp(),
   modified_at timestamptz NOT NULL DEFAULT clock_timestamp(),
+  deleted_at timestamptz DEFAULT NULL,
 
   version bigint NOT NULL DEFAULT 0,
 
   "user" uuid NOT NULL REFERENCES users(id),
-  name text NOT NULL
+  name text NOT NULL,
+
+  deleted boolean NOT NULL DEFAULT false
 ) WITH (OIDS=FALSE);
 
 CREATE TRIGGER image_collections_occ_tgr BEFORE UPDATE ON image_collections
@@ -18,13 +21,25 @@ CREATE TRIGGER image_collections_modified_at_tgr BEFORE UPDATE ON image_collecti
 CREATE TABLE IF NOT EXISTS images (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at timestamptz NOT NULL DEFAULT clock_timestamp(),
+  modified_at timestamptz NOT NULL DEFAULT clock_timestamp(),
+  deleted_at timestamptz DEFAULT NULL,
 
+  version bigint NOT NULL DEFAULT 0,
   "user" uuid NOT NULL REFERENCES users(id),
+
   collection uuid REFERENCES image_collections(id)
                   ON DELETE SET NULL
                   DEFAULT NULL,
 
   name text NOT NULL,
-  size bigint NOT NULL,
   path text NOT NULL
 ) WITH (OIDS=FALSE);
+
+CREATE INDEX ON images (collection);
+
+CREATE TRIGGER images_occ_tgr BEFORE UPDATE ON images
+  FOR EACH ROW EXECUTE PROCEDURE handle_occ();
+
+CREATE TRIGGER images_modified_at_tgr BEFORE UPDATE ON images
+  FOR EACH ROW EXECUTE PROCEDURE update_modified_at();
+
