a602e0846333d8d1c52492c4b03b946f849b71ae
Andrey Antukh <niwi@niwi.nz>
2016-05-07 17:48:57 +0300

    Add basic function for create thumbnails.

--- project.clj
+++ project.clj
@@ -20,6 +20,7 @@
 
                  [instaparse "1.4.1"]
                  [hiccup "1.0.5"]
+                 [org.im4java/im4java "1.4.0"]
 
                  [org.slf4j/slf4j-simple "1.7.21"]
                  [com.layerware/hugsql-core "0.4.7"
---
+++ src/uxbox/util/images.clj
@@ -0,0 +1,39 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
+(ns uxbox.util.images
+  "Images transformation utils."
+  (:require [clojure.java.io :as io])
+  (:import org.im4java.core.IMOperation
+           org.im4java.core.ConvertCmd
+           org.im4java.process.Pipe
+           java.io.ByteArrayInputStream
+           java.io.ByteArrayOutputStream))
+
+(defn thumbnail
+  ([input] (thumbnail input nil))
+  ([input {:keys [size crop format]
+           :or {format "webp"
+                size [200 200]
+                crop true}
+           :as opts}]
+   {:pre [(vector? size)]}
+   (with-open [out (ByteArrayOutputStream.)]
+     (let [[width height] size
+           in (io/input-stream input)
+           pipe-in (Pipe. in nil)
+           pipe-out (Pipe. nil out)
+           op (doto (IMOperation.)
+                (.addImage (into-array String ["-"]))
+                (.thumbnail (int width) (int height))
+                (.gravity "center")
+                (.extent (int width) (int height))
+                (.addImage (into-array String [(str format ":-")])))
+           cmd (doto (ConvertCmd.)
+                 (.setInputProvider pipe-in)
+                 (.setOutputConsumer pipe-out))]
+       (.run cmd op (into-array Object []))
+       (ByteArrayInputStream. (.toByteArray out))))))
