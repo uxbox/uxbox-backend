5f540329876aa26a3a79e5680e688cfb2fd2b273
Andrey Antukh <niwi@niwi.nz>
2016-05-07 13:35:35 +0300

    Add paths helpers namespace.

---
+++ src/uxbox/util/paths.clj
@@ -0,0 +1,71 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
+;;
+;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
+
+(ns uxbox.util.paths
+  "Paths related utils."
+  (:refer-clojure :exclude [name])
+  (:require [storages.core :as st])
+  (:import java.nio.file.Files
+           java.nio.file.Path
+           java.nio.file.Paths
+           java.nio.file.SimpleFileVisitor
+           java.nio.file.FileVisitResult
+           java.nio.file.LinkOption
+           java.nio.file.attribute.FileAttribute
+           java.nio.file.attribute.PosixFilePermissions
+           java.nio.file.NoSuchFileException
+           org.apache.commons.io.FilenameUtils
+           ratpack.form.UploadedFile))
+
+(def path
+  "Alias to storages.core/path."
+  st/path)
+
+(defn exists?
+  "Check if a path exists."
+  [path]
+  (let [^Path path (st/path path)
+        opts (into-array LinkOption [LinkOption/NOFOLLOW_LINKS])]
+    (Files/exists path opts)))
+
+(defn- delete-recursive
+  [^Path path]
+  (try
+    (Files/walkFileTree path (proxy [SimpleFileVisitor] []
+                               (visitFile [file attrs]
+                                 (Files/deleteIfExists file)
+                                 (FileVisitResult/CONTINUE))
+                               (postVisitDirectory [dir exc]
+                                 (Files/deleteIfExists dir)
+                                 (FileVisitResult/CONTINUE))))
+    true
+    (catch NoSuchFileException e
+      false)))
+
+(defn delete
+  "Delete a file or directory."
+  ([path] (delete path nil))
+  ([path {:keys [recursive] :or {recursive false}}]
+   (let [^Path path (st/path path)]
+     (if recursive
+       (delete-recursive path)
+       (Files/deleteIfExists path)))))
+
+(defn extension
+  "Get the file extension."
+  [path]
+  (let [path (st/path path)]
+    (FilenameUtils/getExtension (str path))))
+
+(defn name
+  "Get the file name."
+  [path]
+  (if (instance? UploadedFile path)
+    (.getFileName ^UploadedFile path)
+    (let [path (st/path path)]
+      (FilenameUtils/getName (str path)))))
+
+
