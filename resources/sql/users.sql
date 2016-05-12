ffced75420ed76972899194e80910014f1ff2e31
Andrey Antukh <niwi@niwi.nz>
2016-05-12 22:39:34 +0300

    Add update user photo sql function.

--- resources/sql/users.sql
+++ resources/sql/users.sql
@@ -1,30 +1,31 @@
 -- :name create-profile :<! :1
--- :doc Create a new user entry.
-insert into users (id, fullname, username, email, password, metadata)
-  values (:id, :fullname, :username, :email, :password, :metadata)
-  returning *;
+insert into users (id, fullname, username, email, password, metadata, photo)
+values (:id, :fullname, :username, :email, :password, :metadata, '')
+returning *;
 
 -- :name get-profile :? :1
--- :doc Retrieve the profile data.
 select * from users
-  where id = :id and deleted = false;
+ where id = :id and deleted = false;
 
 -- :name get-profile-by-username :? :1
--- :doc Retrieve the profile data.
 select * from users
-  where (username = :username or email = :username)
-        and deleted = false;
+ where (username = :username or email = :username)
+   and deleted = false;
 
 -- :name update-profile :<! :1
--- :doc Update profile.
-update users set username = :username,
-                 email = :email,
-                 fullname = :fullname,
-                 metadata = :metadata
-  where id = :id and deleted = false
-  returning *;
+update users
+   set username = :username,
+       email = :email,
+       fullname = :fullname,
+       metadata = :metadata
+ where id = :id and deleted = false
+returning *;
 
 -- :name update-profile-password :! :n
--- :doc Update profile password
 update users set password = :password
-  where id = :id and deleted = false;
+ where id = :id and deleted = false;
+
+-- :name update-profile-photo :! :n
+update users set photo = :photo
+ where id = :id and deleted = false;
+
