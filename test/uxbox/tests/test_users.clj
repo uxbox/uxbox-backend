f6a8452e596094a7ecfc5ad68fa14a352fb2f088
Andrey Antukh <niwi@niwi.nz>
2016-05-12 22:29:26 +0300

    Split monolithic users tests in two separated tests.

--- test/uxbox/tests/test_users.clj
+++ test/uxbox/tests/test_users.clj
@@ -12,22 +12,25 @@
 
 (t/use-fixtures :each th/database-reset)
 
-(t/deftest test-http-update-and-retrieve-profile
+(t/deftest test-http-retrieve-profile
   (with-open [conn (up/get-conn)]
     (let [user (th/create-user conn 1)]
       ;; Retrieve
       (with-server {:handler (urt/app)}
         (let [uri (str th/+base-url+ "/api/profile/me")
               [status data] (th/http-get user uri)]
-          ;; (println "RESPONSE:" status data)
+          (println "RESPONSE:" status data)
           (t/is (= 200 status))
           (t/is (= (:fullname data) "User 1"))
           (t/is (= (:username data) "user1"))
           (t/is (= (:metadata data) "1"))
           (t/is (= (:email data) "user1@uxbox.io"))
-          (t/is (not (contains? data :password)))))
+          (t/is (not (contains? data :password))))))))
 
-      ;; Update
+;; Update
+(t/deftest test-http-update-profile
+  (with-open [conn (up/get-conn)]
+    (let [user (th/create-user conn 1)]
       (with-server {:handler (urt/app)}
         (let [uri (str th/+base-url+ "/api/profile/me")
               data (assoc user
@@ -43,5 +46,3 @@
           (t/is (= (:metadata data) "222"))
           (t/is (= (:email data) "user222@uxbox.io"))
           (t/is (not (contains? data :password))))))))
-
-
