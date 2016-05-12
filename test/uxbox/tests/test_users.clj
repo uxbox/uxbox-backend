58ed2e219c12e838d6ba8d5083f66f2b74650f77
Andrey Antukh <niwi@niwi.nz>
2016-05-12 22:40:26 +0300

    Add tests.

--- test/uxbox/tests/test_users.clj
+++ test/uxbox/tests/test_users.clj
@@ -1,5 +1,6 @@
 (ns uxbox.tests.test-users
   (:require [clojure.test :as t]
+            [clojure.java.io :as io]
             [promesa.core :as p]
             [clj-http.client :as http]
             [catacumba.testing :refer (with-server)]
@@ -15,7 +16,6 @@
 (t/deftest test-http-retrieve-profile
   (with-open [conn (up/get-conn)]
     (let [user (th/create-user conn 1)]
-      ;; Retrieve
       (with-server {:handler (urt/app)}
         (let [uri (str th/+base-url+ "/api/profile/me")
               [status data] (th/http-get user uri)]
@@ -27,7 +27,6 @@
           (t/is (= (:email data) "user1@uxbox.io"))
           (t/is (not (contains? data :password))))))))
 
-;; Update
 (t/deftest test-http-update-profile
   (with-open [conn (up/get-conn)]
     (let [user (th/create-user conn 1)]
@@ -46,3 +45,18 @@
           (t/is (= (:metadata data) "222"))
           (t/is (= (:email data) "user222@uxbox.io"))
           (t/is (not (contains? data :password))))))))
+
+
+(t/deftest test-http-update-profile-photo
+  (with-open [conn (up/get-conn)]
+    (let [user (th/create-user conn 1)]
+      (with-server {:handler (urt/app)}
+        (let [uri (str th/+base-url+ "/api/profile/me/photo")
+              params [{:name "sample.jpg"
+                       :part-name "file"
+                       :content (io/input-stream
+                                 (io/resource "uxbox/tests/_files/sample.jpg"))}]
+              [status data] (th/http-multipart user uri params)]
+          (println "RESPONSE:" status data)
+          (t/is (= 204 status)))))))
+
