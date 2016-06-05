d25f7034e46cd9f936a9e36b2d3f63eaccbfb0a2
Andrey Antukh <niwi@niwi.nz>
2016-06-05 13:15:39 +0300

    Update to clojure 1.9.0-alpha4.

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
              ;; "-Dcom.sun.management.jmxremote.rmi.port=9090"
              ;; "-Djava.rmi.server.hostname=0.0.0.0"
              "-XX:+UseG1GC" "-Xms1g" "-Xmx1g"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [funcool/struct "0.1.0"]
                  [funcool/suricatta "1.1.0"]
