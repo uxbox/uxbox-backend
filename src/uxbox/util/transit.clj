1a1514eb2e60a4f464790920f2b5a7ddd3ad7b71
Andrey Antukh <niwi@niwi.nz>
2016-06-05 13:18:31 +0300

    Add streaming transit encoding support.

--- src/uxbox/util/transit.clj
+++ src/uxbox/util/transit.clj
@@ -5,13 +5,50 @@
 ;; Copyright (c) 2016 Andrey Antukh <niwi@niwi.nz>
 
 (ns uxbox.util.transit
-  (:require [catacumba.serializers :as sz]
-            [uxbox.util.time :as dt]))
+  (:require [cognitect.transit :as t]
+            [uxbox.util.time :as dt])
+  (:import java.io.ByteArrayInputStream
+           java.io.ByteArrayOutputStream))
+
+(def ^:private +reader-handlers+
+  dt/+read-handlers+)
+
+(def ^:private +write-handlers+
+  dt/+write-handlers+)
+
+(defn reader
+  ([istream]
+   (reader istream nil))
+  ([istream {:keys [type] :or {type :json}}]
+   (t/reader istream type {:handlers +reader-handlers+})))
+
+(defn read!
+  "Read value from streamed transit reader."
+  [reader]
+  (t/read reader))
+
+(defn writer
+  ([ostream]
+   (writer ostream nil))
+  ([ostream {:keys [type] :or {type :json}}]
+   (t/writer ostream type {:handlers +write-handlers+})))
+
+(defn write!
+  [writer data]
+  (t/write writer data))
 
 (defn decode
-  [data]
-  (sz/decode data :transit+json {:handlers dt/+read-handlers+}))
+  ([data]
+   (decode data nil))
+  ([data opts]
+   (with-open [input (ByteArrayInputStream. data)]
+     (read! (reader input opts)))))
 
 (defn encode
-  [data]
-  (sz/encode data :transit+json {:handlers dt/+write-handlers+}))
+  ([data]
+   (encode data nil))
+  ([data opts]
+   (with-open [out (ByteArrayOutputStream.)]
+     (let [w (writer out opts)]
+       (write! w data)
+       (.toByteArray out)))))
