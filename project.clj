138433a56b0e7358c994edd371106fb6b021b887
Andrey Antukh <niwi@niwi.nz>
2016-10-01 22:59:45 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -13,12 +13,14 @@
              "-Dclojure.spec.check-asserts=true"
              "-Dclojure.spec.compile-asserts=true"
              "-XX:+UseG1GC" "-Xms1g" "-Xmx1g"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [org.clojure/tools.logging "0.3.1"]
                  [funcool/struct "1.0.0"]
                  [funcool/suricatta "1.1.0"]
-                 [funcool/promesa "1.3.1"]
-                 [funcool/catacumba "1.0.0-SNAPSHOT"]
+                 [funcool/promesa "1.5.0"]
+                 [funcool/catacumba "1.1.1"]
+
+                 [org.clojure/data.xml "0.1.0-beta2"]
 
                  [hiccup "1.0.5"]
                  [org.im4java/im4java "1.4.0"]
@@ -28,17 +30,17 @@
                   :exclusions [org.clojure/tools.reader]]
                  [niwinz/migrante "0.1.0"]
 
-                 [buddy/buddy-sign "1.1.0" :exclusions [org.clojure/tools.reader]]
-                 [buddy/buddy-hashers "0.14.0"]
+                 [buddy/buddy-sign "1.2.0" :exclusions [org.clojure/tools.reader]]
+                 [buddy/buddy-hashers "1.0.0"]
 
                  [org.xerial.snappy/snappy-java "1.1.2.6"]
-                 [com.github.spullara.mustache.java/compiler "0.9.2"]
-                 [org.postgresql/postgresql "9.4.1208"]
+                 [com.github.spullara.mustache.java/compiler "0.9.4"]
+                 [org.postgresql/postgresql "9.4.1211"]
                  [org.quartz-scheduler/quartz "2.2.3"]
                  [org.quartz-scheduler/quartz-jobs "2.2.3"]
                  [commons-io/commons-io "2.5"]
-                 [com.draines/postal "2.0.0"]
+                 [com.draines/postal "2.0.1"]
 
-                 [hikari-cp "1.7.1"]
+                 [hikari-cp "1.7.4"]
                  [mount "0.1.10"]
-                 [environ "1.0.3"]])
+                 [environ "1.1.0"]])
