a1ad34c77d15612238296c5b0ae15b621c57fdcc
Andrey Antukh <niwi@niwi.nz>
2016-11-18 19:37:01 +0100

    Remove commented code from scripts/run.sh script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-# JVM_OPTS="-Xms4g -Xmx4g -XX:+UseG1GC -XX:+AggressiveOpts -server"
-# java $JVM_OPTS -jar target/uxbox-backend-0.1.0-SNAPSHOT-standalone.jar -m uxbox.main
 export PATH=/opt/img/bin:$PATH
 lein trampoline run -m uxbox.main
