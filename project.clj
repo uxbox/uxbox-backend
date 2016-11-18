9b9aa7a65f42899724eea3707f5784351b968414
Andrey Antukh <niwi@niwi.nz>
2016-11-18 19:36:55 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -14,12 +14,12 @@
              "-Dclojure.spec.compile-asserts=true"
              "-XX:+UseG1GC" "-Xms1g" "-Xmx1g"]
   :global-vars {*assert* true}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/tools.logging "0.3.1"]
                  [funcool/struct "1.0.0"]
-                 [funcool/suricatta "1.1.0"]
-                 [funcool/promesa "1.5.0"]
-                 [funcool/catacumba "1.1.1"]
+                 [funcool/suricatta "1.2.0"]
+                 [funcool/promesa "1.6.0"]
+                 [funcool/catacumba "1.2.0"]
 
                  [org.clojure/data.xml "0.1.0-beta2"]
 
@@ -31,17 +31,17 @@
                   :exclusions [org.clojure/tools.reader]]
                  [niwinz/migrante "0.1.0"]
 
-                 [buddy/buddy-sign "1.2.0" :exclusions [org.clojure/tools.reader]]
-                 [buddy/buddy-hashers "1.0.0"]
+                 [buddy/buddy-sign "1.3.0" :exclusions [org.clojure/tools.reader]]
+                 [buddy/buddy-hashers "1.1.0"]
 
                  [org.xerial.snappy/snappy-java "1.1.2.6"]
                  [com.github.spullara.mustache.java/compiler "0.9.4"]
-                 [org.postgresql/postgresql "9.4.1211"]
+                 [org.postgresql/postgresql "9.4.1212"]
                  [org.quartz-scheduler/quartz "2.2.3"]
                  [org.quartz-scheduler/quartz-jobs "2.2.3"]
                  [commons-io/commons-io "2.5"]
-                 [com.draines/postal "2.0.1"]
+                 [com.draines/postal "2.0.2"]
 
-                 [hikari-cp "1.7.4"]
+                 [hikari-cp "1.7.5"]
                  [mount "0.1.10"]
                  [environ "1.1.0"]])
